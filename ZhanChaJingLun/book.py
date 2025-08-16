9dac7d6f38670a853684adac2675b4c44fbc4219
emptist <jigme1968@gmail.com>
2025-08-16 11:27:39 +0800

    modified

--- ZhanChaJingLun/book.py
+++ ZhanChaJingLun/book.py
@@ -3,7 +3,7 @@ from streamlit_autorefresh import st_autorefresh
 from pathlib import Path
 
 # Set up autorefresh to run every 2000 milliseconds (2 seconds)
-st_autorefresh(interval=2000, key="book_md_refresh")
+st_autorefresh(interval=60000, key="book_md_refresh")
 
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
